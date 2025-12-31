e0fb7cec875eb976642415c64c9b96a1622609fd
lutit <244531230+lutit@users.noreply.github.com>
2026-01-01 00:56:51 +0200

    fix time

--- Telegram/SourceFiles/history/view/history_view_element.cpp
+++ Telegram/SourceFiles/history/view/history_view_element.cpp
@@ -622,7 +622,7 @@ QString DateTooltipText(not_null<Element*> view) {
 					dateText += '\n' + tr::lng_forwarded_saved_date(
 						tr::now,
 						lt_date,
-						locale.toString(parsed, format));
+						formatDateTime(parsed));
 				}
 			}
 		}
